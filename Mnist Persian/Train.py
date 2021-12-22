16c43966948596362fa0270caabc5256a3a5a2c4
Ali Heravi <56476428+maheravi@users.noreply.github.com>
2021-12-22 18:00:12 +0330

    Add files via upload

---
+++ Mnist Persian/Train.py
@@ -0,0 +1,78 @@
+import torch
+import torchvision
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision.datasets import FashionMNIST
+from Model import MyModel
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--device", default='cpu', type=str)
+parser.add_argument("--dataset", type=str)
+
+args = parser.parse_args()
+
+latent_size = 10
+disc_inp_sz = 28*28
+img_size = 28
+epochs = 10
+batch_size = 32
+lr = 0.001
+
+device = torch.device(args.device)
+
+model = MyModel(disc_inp_sz).to(device)
+
+model = model.to(device)
+model.train(True)
+
+
+def calc_acc(preds, labels):
+    _, preds_max = torch.max(preds, 1)
+    acc = torch.sum(preds_max == labels.data, dtype=torch.float64) / len(preds)
+    return acc
+
+
+# Data Preparing
+
+transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((28, 28)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+])
+
+dataset = torchvision.datasets.ImageFolder(root=args.dataset, transform=transform)
+train_data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+# compile
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+loss_function = torch.nn.CrossEntropyLoss()
+
+# train
+
+for epoch in range(1, epochs + 1):
+    train_loss = 0.0
+    train_acc = 0.0
+    for images, labels in train_data_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        optimizer.zero_grad()
+        # 1- forwarding
+        preds = model(images)
+        # 2- backwarding
+        loss = loss_function(preds, labels)
+        loss.backward()
+        # 3- Update
+        optimizer.step()
+
+        train_loss += loss
+        train_acc += calc_acc(preds, labels)
+
+    total_loss = train_loss / len(train_data_loader)
+    total_acc = train_acc / len(train_data_loader)
+
+    print(f"Epoch: {epoch}, Loss: {total_loss}, Acc: {total_acc}")
+
+# save
+torch.save(model.state_dict(), "PersianMnistFinal.pth")
